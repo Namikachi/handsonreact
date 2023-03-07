b1be40f908c6c50db62eeef1a5ca16b58030011a
Panorami <loud.music.n@gmail.com>
2023-03-06 19:38:33 -0500

    third commit
    - Lab 9

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,11 +1,20 @@
 import { Project } from './Project';
+import ProjectCard from './ProjectCard';
 
 interface ProjectListProps {
 	projects: Project[];
 }
 
 function ProjectList({ projects }: ProjectListProps ) {
-	return <pre>{JSON.stringify(projects, null, ' ')}</pre>;
+	return (
+		<div className="row">
+			{projects.map((project) => (
+				<div className="cols-sm" key={project.id}>
+					<ProjectCard project={project}/>
+				</div>
+			))}
+		</div>
+	)
 }
 
 export default ProjectList;
