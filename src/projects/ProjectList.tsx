c4aca484b609e0ea6fc4f5f45d20633427977dbc
Panorami <loud.music.n@gmail.com>
2023-03-07 19:50:39 -0500

    fourth commit
    - Lab 11

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,16 +1,21 @@
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
 
 interface ProjectListProps {
 	projects: Project[];
 }
 
 function ProjectList({ projects }: ProjectListProps ) {
+	const handleEdit = (project: Project) => {
+		console.log(project);
+	};
 	return (
 		<div className="row">
 			{projects.map((project) => (
 				<div className="cols-sm" key={project.id}>
-					<ProjectCard project={project}/>
+					<ProjectCard project={project} onEdit={handleEdit} />
+					<ProjectForm />
 				</div>
 			))}
 		</div>
