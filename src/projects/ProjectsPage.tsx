2483d582a5d8392da7c3ecef21cc0f1d6b2aab83
Panorami <loud.music.n@gmail.com>
2023-03-12 19:29:21 -0400

    9th commit
    - Lab 18

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -7,31 +7,38 @@ function ProjectsPage() {
 	const [projects, setProjects] = useState<Project[]>([]);
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState<string | undefined>(undefined);
+	const [currentPage, setCurrentPage] = useState(1);
 
 	useEffect(() => {
 		async function loadProjects() {
 			setLoading(true);
 			try {
-				const data = await projectAPI.get(1);
-				setError('');
-				setProjects(data);
-			}
-				catch (e) {
+				const data = await projectAPI.get(currentPage);
+				if (currentPage === 1) {
+					setProjects(data);
+				} else {
+					setProjects((projects) => [...projects, ...data]);
+				}
+			} catch (e) {
 				if (e instanceof Error) {
 					setError(e.message);
 				}
-				} finally {
+			} finally {
 				setLoading(false);
 			}
 		}
 		loadProjects();
-	}, []);
+	}, [currentPage]);
 
 	const saveProject = (project: Project) => {
 		let updatedProjects = projects.map((p: Project) => {
 			return p.id === project.id ? project : p;
 		});
 		setProjects(updatedProjects);
+	};
+
+	const handleMoreClick = () => {
+		setCurrentPage((currentPage) => currentPage + 1);
 	};
 
 	return (
@@ -51,6 +58,17 @@ function ProjectsPage() {
 			)}
 
 			<ProjectList onSave={saveProject} projects={projects} />
+			{!loading && !error && (
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="button-group fluid">
+              <button className="button default" onClick={handleMoreClick}>
+                More...
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
 			{loading && (
 				<div className="center-page">
 					<span className="spinner primary"></span>
--- src/projects/projectAPI.ts
+++ src/projects/projectAPI.ts
@@ -51,7 +51,7 @@ function convertToProjectModel(item: any): Project {
 const projectAPI = {
 	get(page = 1, limit = 20) {
 		return fetch(`${url}?_page=${page}&_limit=${limit}&_sort=name`)
-			.then(delay(600))
+			// .then(delay(600))
 			.then(checkStatus)
 			.then(parseJSON)
 			.then(convertToProjectModels)
